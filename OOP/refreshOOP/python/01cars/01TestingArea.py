ba65e53dfbca24e9a597dafb01adf1b6ac6c7847
JianoFreo <jianofreo.magtangob@tup.edu.ph>
2026-02-25 02:29:13 +0800

    separated the main class from testing file

---
+++ OOP/refreshOOP/python/01cars/01TestingArea.py
@@ -0,0 +1,7 @@
+from MainClass import Car
+
+car1 = Car("Mustang", 2024, "red", False)
+car2 = Car("Corvette", 2025, "blue", True)
+
+print(car1.model)
+print(car2.year)
--- OOP/refreshOOP/python/01cars/MainClass.py
+++ OOP/refreshOOP/python/01cars/MainClass.py
@@ -4,9 +4,3 @@ class Car:
         self.year = year
         self.color = color
         self.for_sale = for_sale
-
-car1 = Car("Mustang", 2024, "red", False)
-car2 = Car("Corvette", 2025, "blue", True)
-
-print(car1.model)
-print(car2.year)
