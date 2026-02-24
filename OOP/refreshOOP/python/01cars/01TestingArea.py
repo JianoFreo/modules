62b5cf2b9d267ab6b65d25bedd1a5b0187729669
JianoFreo <jianofreo.magtangob@tup.edu.ph>
2026-02-25 02:36:15 +0800

    add drive and stop methods to Car class and update test script

--- OOP/refreshOOP/python/01cars/01TestingArea.py
+++ OOP/refreshOOP/python/01cars/01TestingArea.py
@@ -2,6 +2,9 @@ from MainClass import Car
 
 car1 = Car("Mustang", 2024, "red", False)
 car2 = Car("Corvette", 2025, "blue", True)
-
+##-------------------new method--------------------
 print(car1.model)
 print(car2.year)
+##-------------------new method--------------------
+car1.drive()
+car2.stop()
